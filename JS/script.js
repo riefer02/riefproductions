fcc22f72dd1e791790a939edbf3db6c69a8e1a34
Andrew Riefenstahl <andrew.riefenstahl@gmail.com>
2020-04-28 13:09:05 -0500

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+console.log(69);
+
+var cuteDog = "Dayzee";
+
+const andrew = {
+	firstName: 'Andrew',
+	lastName: 'Riefenstahl',
+	dateOfBirth: 'November 9th, 1988',
+	instrument: 'Vox/Guitar',
+};
